b0ccd60e1c528e3c6467948195bb2ebab72558e4
Julien Vaubourg <julien@vaubourg.com>
2015-10-08 20:56:44 +0200

    * install-sd: HTTPS downloads and using of curl instead of wget (problems with wget < 1.16) * Add Automatic tab screenshot

--- install-sd.sh
+++ install-sd.sh
@@ -65,7 +65,7 @@ function show_usage() {
   echo -e "     Install an image for LIME2" >&2
   echo -e "     \e[2mDefault: LIME\e[0m" >&2
   echo -e "  \e[1m-l\e[0m" >&2
-  echo -e "     Just scan network for finding Internet Cube local IPv4s" >&2
+  echo -e "     Just scan network for finding local IPv4s corresponding to Internet Cubes" >&2
   echo -e "  \e[1m-d\e[0m" >&2
   echo -e "     Enable debug messages" >&2
   echo -e "  \e[1m-h\e[0m" >&2
@@ -129,13 +129,15 @@ function check_sudo() {
     exit_error "sudo command is required"
   fi
 
+  info "This script needs a sudo access"
+
   if ! sudo echo &> /dev/null; then
     exit_error "sudo password is required"
   fi
 }
 
 function check_bins() {
-  local bins=(wget tar awk md5sum mountpoint cryptsetup parted mke2fs tune2fs losetup)
+  local bins=(curl tar awk md5sum mountpoint cryptsetup parted mke2fs tune2fs losetup)
 
   for i in "${bins[@]}"; do
     if ! sudo which "${i}" &> /dev/null; then
@@ -293,6 +295,7 @@ function find_cubes() {
 function autodetect_sdcardpath() {
   echo -n "1. Please, remove the target SD card from your computer if present, then press Enter"
   read -s && echo
+  sleep 2
 
   local blocks1=$(ls -1 /sys/block/)
 
@@ -300,13 +303,13 @@ function autodetect_sdcardpath() {
 
   echo -n "2. Please, plug the target SD card into your computer (and don't touch to other devices), then press Enter"
   read -s && echo
+  sleep 2
 
-  sleep 1
   local blocks2=$(ls -1 /sys/block/)
   local foundblock=;
   local confirm=no;
 
-  debug "Block devices found: $(echo $blocks1)"
+  debug "Block devices found: $(echo $blocks2)"
 
   for i in $blocks2; do
     if ! (echo ' '${blocks1}' ' | grep -q " ${i} ") && [ -b "/dev/${i}" ]; then
@@ -339,9 +342,9 @@ function download_img() {
   
   local tar_name="labriqueinternet_A20LIME${urlpart_lime2}${urlpart_encryptedfs}_latest_${deb_version}.img.tar.xz"
 
-  debug "Downloading ${url_base}${tar_name}"
+  info "Downloading ${tar_name}..."
 
-  if ! wget -q --show-progress "${url_base}${tar_name}" -P "${tmp_dir}"; then
+  if ! download_file "${url_base}${tar_name}" "${tmp_dir}"; then
     exit_error "Image download failed"
   fi
 
@@ -364,9 +367,7 @@ function untar_img() {
 }
 
 function download_md5() {
-  debug "Downloading ${url_base}MD5SUMS"
-
-  if ! wget -q --show-progress "${url_base}MD5SUMS" -P "${tmp_dir}"; then
+  if ! download_file "${url_base}MD5SUMS" "${tmp_dir}"; then
     exit_error "MD5SUMS file download failed"
   fi
 
@@ -395,6 +396,65 @@ function check_md5() {
   fi
 }
 
+function download_file() {
+  local url=$1
+  local dest_dir=$2
+
+  if [ ! -f "${tmp_dir}/cacert.crt" ]; then
+  cat << EOF > "${tmp_dir}/cacert_root.crt"
+-----BEGIN CERTIFICATE-----
+MIIHPTCCBSWgAwIBAgIBADANBgkqhkiG9w0BAQQFADB5MRAwDgYDVQQKEwdSb290
+IENBMR4wHAYDVQQLExVodHRwOi8vd3d3LmNhY2VydC5vcmcxIjAgBgNVBAMTGUNB
+IENlcnQgU2lnbmluZyBBdXRob3JpdHkxITAfBgkqhkiG9w0BCQEWEnN1cHBvcnRA
+Y2FjZXJ0Lm9yZzAeFw0wMzAzMzAxMjI5NDlaFw0zMzAzMjkxMjI5NDlaMHkxEDAO
+BgNVBAoTB1Jvb3QgQ0ExHjAcBgNVBAsTFWh0dHA6Ly93d3cuY2FjZXJ0Lm9yZzEi
+MCAGA1UEAxMZQ0EgQ2VydCBTaWduaW5nIEF1dGhvcml0eTEhMB8GCSqGSIb3DQEJ
+ARYSc3VwcG9ydEBjYWNlcnQub3JnMIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIIC
+CgKCAgEAziLA4kZ97DYoB1CW8qAzQIxL8TtmPzHlawI229Z89vGIj053NgVBlfkJ
+8BLPRoZzYLdufujAWGSuzbCtRRcMY/pnCujW0r8+55jE8Ez64AO7NV1sId6eINm6
+zWYyN3L69wj1x81YyY7nDl7qPv4coRQKFWyGhFtkZip6qUtTefWIonvuLwphK42y
+fk1WpRPs6tqSnqxEQR5YYGUFZvjARL3LlPdCfgv3ZWiYUQXw8wWRBB0bF4LsyFe7
+w2t6iPGwcswlWyCR7BYCEo8y6RcYSNDHBS4CMEK4JZwFaz+qOqfrU0j36NK2B5jc
+G8Y0f3/JHIJ6BVgrCFvzOKKrF11myZjXnhCLotLddJr3cQxyYN/Nb5gznZY0dj4k
+epKwDpUeb+agRThHqtdB7Uq3EvbXG4OKDy7YCbZZ16oE/9KTfWgu3YtLq1i6L43q
+laegw1SJpfvbi1EinbLDvhG+LJGGi5Z4rSDTii8aP8bQUWWHIbEZAWV/RRyH9XzQ
+QUxPKZgh/TMfdQwEUfoZd9vUFBzugcMd9Zi3aQaRIt0AUMyBMawSB3s42mhb5ivU
+fslfrejrckzzAeVLIL+aplfKkQABi6F1ITe1Yw1nPkZPcCBnzsXWWdsC4PDSy826
+YreQQejdIOQpvGQpQsgi3Hia/0PsmBsJUUtaWsJx8cTLc6nloQsCAwEAAaOCAc4w
+ggHKMB0GA1UdDgQWBBQWtTIb1Mfz4OaO873SsDrusjkY0TCBowYDVR0jBIGbMIGY
+gBQWtTIb1Mfz4OaO873SsDrusjkY0aF9pHsweTEQMA4GA1UEChMHUm9vdCBDQTEe
+MBwGA1UECxMVaHR0cDovL3d3dy5jYWNlcnQub3JnMSIwIAYDVQQDExlDQSBDZXJ0
+IFNpZ25pbmcgQXV0aG9yaXR5MSEwHwYJKoZIhvcNAQkBFhJzdXBwb3J0QGNhY2Vy
+dC5vcmeCAQAwDwYDVR0TAQH/BAUwAwEB/zAyBgNVHR8EKzApMCegJaAjhiFodHRw
+czovL3d3dy5jYWNlcnQub3JnL3Jldm9rZS5jcmwwMAYJYIZIAYb4QgEEBCMWIWh0
+dHBzOi8vd3d3LmNhY2VydC5vcmcvcmV2b2tlLmNybDA0BglghkgBhvhCAQgEJxYl
+aHR0cDovL3d3dy5jYWNlcnQub3JnL2luZGV4LnBocD9pZD0xMDBWBglghkgBhvhC
+AQ0ESRZHVG8gZ2V0IHlvdXIgb3duIGNlcnRpZmljYXRlIGZvciBGUkVFIGhlYWQg
+b3ZlciB0byBodHRwOi8vd3d3LmNhY2VydC5vcmcwDQYJKoZIhvcNAQEEBQADggIB
+ACjH7pyCArpcgBLKNQodgW+JapnM8mgPf6fhjViVPr3yBsOQWqy1YPaZQwGjiHCc
+nWKdpIevZ1gNMDY75q1I08t0AoZxPuIrA2jxNGJARjtT6ij0rPtmlVOKTV39O9lg
+18p5aTuxZZKmxoGCXJzN600BiqXfEVWqFcofN8CCmHBh22p8lqOOLlQ+TyGpkO/c
+gr/c6EWtTZBzCDyUZbAEmXZ/4rzCahWqlwQ3JNgelE5tDlG+1sSPypZt90Pf6DBl
+Jzt7u0NDY8RD97LsaMzhGY4i+5jhe1o+ATc7iwiwovOVThrLm82asduycPAtStvY
+sONvRUgzEv/+PDIqVPfE94rwiCPCR/5kenHA0R6mY7AHfqQv0wGP3J8rtsYIqQ+T
+SCX8Ev2fQtzzxD72V7DX3WnRBnc0CkvSyqD/HMaMyRa+xMwyN2hzXwj7UfdJUzYF
+CpUCTPJ5GhD22Dp1nPMd8aINcGeGG7MW9S/lpOt5hvk9C8JzC6WZrG/8Z7jlLwum
+GCSNe9FINSkYQKyTYOGWhlC0elnYjyELn8+CkcY7v2vcB5G5l1YjqrZslMZIBjzk
+zk6q5PYvCdxTby78dOs6Y5nCpqyJvKeyRKANihDjbPIky/qbn3BHLt4Ui9SyIAmW
+omTxJBzcoTWcFbLUvFUufQb1nA5V9FrWk9p2rSVzTMVD
+-----END CERTIFICATE-----
+EOF
+  fi
+
+  debug "Downloading ${url}"
+
+  if ! (cd "${dest_dir}" && curl -#fOA SdCardInstaller --cacert "${tmp_dir}/cacert_root.crt" "${url}"); then
+    return 1
+  fi
+
+  return 0
+}
+
 
 ######################
 ### CORE FUNCTIONS ###
@@ -420,6 +480,7 @@ function install_encrypted() {
 
   confirm_writing
 
+  info "Please wait..."
   debug "Partitioning ${opt_sdcardpath}"
 
   sudo parted --script "${opt_sdcardpath}" mklabel msdos
@@ -517,6 +578,8 @@ function install_encrypted() {
 function install_clear() {
   confirm_writing
 
+  info "Please wait..."
+
   debug "Raw copying ${img_path} to ${opt_sdcardpath} (dd)"
   sudo dd "if=${img_path}" of="${opt_sdcardpath}" bs=1M &> /dev/null
 
@@ -529,7 +592,7 @@ function install_clear() {
 ### GLOBAL VARIABLES ###
 ########################
 
-url_base=http://repo.labriqueinter.net/
+url_base=https://repo.labriqueinter.net/
 deb_version=jessie
 opt_encryptedfs=false
 opt_debug=false
@@ -593,11 +656,11 @@ img_path=$opt_imgpath
 md5_path=$opt_md5path
 
 if [ -z "${img_path}" ]; then
-  info "Downloading Debian/YunoHost image"
+  info "Downloading Debian/YunoHost image (HTTPS)"
   download_img
 
   if [ -z "${md5_path}" ]; then
-    info "Downloading MD5SUMS"
+    info "Downloading MD5SUMS (HTTPS)"
     download_md5
   fi
 fi
