69383314875808a4c5004d9c6011952ccb0ee251
opi <opi@zeropi.net>
2015-10-13 12:25:45 +0200

    JS breaks when there's no #show-features-button on the page.

--- js/main.js
+++ js/main.js
@@ -2,14 +2,14 @@
 document.querySelector('body').className += ' has-js ';
 
 // ShowFeatures list button
-document.querySelector('#show-features-button')
-    .addEventListener('click', function(event) {
-        // Prevent scrolling
-        event.preventDefault();
-        // Toggle features
-        var features = document.querySelector('#features').style;
-        features.display = (features.display != 'block') ? 'block' : 'none';
-    }, false);
+var showFeaturesButton = document.querySelector('#show-features-button');
+showFeaturesButton ? showFeaturesButton.addEventListener('click', function(event) {
+    // Prevent scrolling
+    event.preventDefault();
+    // Toggle features
+    var features = document.querySelector('#features').style;
+    features.display = (features.display != 'block') ? 'block' : 'none';
+}, false) : null;
 
 // firefox doesn't display subtitles by default
 var videos = document.getElementsByTagName("video");
