e6248bb5cc456b58e832576e60c963e19f53e71e
Julien Vaubourg <julien@vaubourg.com>
2015-12-30 03:42:41 +0100

    Update

--- generator/js/generator.js
+++ generator/js/generator.js
@@ -106,18 +106,18 @@ var cube = {
     }
 
     var json = {
-      server_name: $('#vpn_server_name').val(),
-      server_port: $('#vpn_server_port').val(),
-      server_proto: $('input[name=vpn_server_proto]:checked').val(),
-      ip6_net: $('#vpn_ip6_net').val(),
+      server_name: $('#vpn_server_name').val().trim(),
+      server_port: $('#vpn_server_port').val().trim(),
+      server_proto: $('input[name=vpn_server_proto]:checked').val().trim(),
+      ip6_net: $('#vpn_ip6_net').val().trim(),
       crt_server_ca: cube.crtFilesContent.crt_server_ca,
       crt_client: cube.crtFilesContent.crt_client,
       crt_client_key: cube.crtFilesContent.crt_client_key,
       crt_client_ta: cube.crtFilesContent.crt_client_ta,
-      login_user: $('#vpn_login_user').val(),
-      login_passphrase: $('#vpn_login_passphrase').val(),
-      dns0: $('#vpn_dns0').val(),
-      dns1: $('#vpn_dns1').val(),
+      login_user: $('#vpn_login_user').val().trim(),
+      login_passphrase: $('#vpn_login_passphrase').val().trim(),
+      dns0: $('#vpn_dns0').val().trim(),
+      dns1: $('#vpn_dns1').val().trim(),
       openvpn_rm: $('#vpn_openvpn_rm').val().split("\n"),
       openvpn_add: $('#vpn_openvpn_add').val().split("\n")
     };
@@ -153,25 +153,25 @@ var hypercube = {
       vpnclient: cubeJson,
 
       hotspot: {
-        wifi_ssid: $('#hotspot_wifi_ssid').val(),
-        wifi_passphrase: $('#hotspot_wifi_passphrase').val(),
-        ip6_net: $('#hotspot_ip6_net').val(),
-        ip6_dns0: $('#hotspot_ip6_dns0').val(),
-        ip6_dns1: $('#hotspot_ip6_dns1').val(),
-        ip4_dns0: $('#hotspot_ip4_dns0').val(),
-        ip4_dns1: $('#hotspot_ip4_dns1').val(),
-        ip4_nat_prefix: $('#hotspot_ip4_nat_prefix').val(),
+        wifi_ssid: $('#hotspot_wifi_ssid').val().trim(),
+        wifi_passphrase: $('#hotspot_wifi_passphrase').val().trim(),
+        ip6_net: $('#hotspot_ip6_net').val().trim(),
+        ip6_dns0: $('#hotspot_ip6_dns0').val().trim(),
+        ip6_dns1: $('#hotspot_ip6_dns1').val().trim(),
+        ip4_dns0: $('#hotspot_ip4_dns0').val().trim(),
+        ip4_dns1: $('#hotspot_ip4_dns1').val().trim(),
+        ip4_nat_prefix: $('#hotspot_ip4_nat_prefix').val().trim(),
         firmware_nonfree: $('#hotspot_firmware_nonfree').is(':checked') ? 'yes' : 'no'
       },
 
       yunohost: {
-        domain: $('#ynh_domain').val(),
-        add_domain: $('#ynh_add_domain').val(),
-        password: $('#ynh_password').val(),
-        user: $('#ynh_user').val(),
-        user_firstname: $('#ynh_user_firstname').val(),
-        user_lastname: $('#ynh_user_lastname').val(),
-        user_password: $('#ynh_user_password').val()
+        domain: $('#ynh_domain').val().trim(),
+        add_domain: $('#ynh_add_domain').val().trim(),
+        password: $('#ynh_password').val().trim(),
+        user: $('#ynh_user').val().trim(),
+        user_firstname: $('#ynh_user_firstname').val().trim(),
+        user_lastname: $('#ynh_user_lastname').val().trim(),
+        user_password: $('#ynh_user_password').val().trim()
       }
     };
 
@@ -258,6 +258,7 @@ var view = {
     $('[data-toggle="tooltip"]').tooltip();
     $('.switch').bootstrapToggle();
 
+    $('form').submit(navigation.formNextSubmit);
     $('.nav-wizard a').click(navigation.timelineClick);
     $('.nav-tabs a').click(navigation.tabClick);
     $('.nav-pills a').click(navigation.questionClick);
@@ -266,8 +267,10 @@ var view = {
     $('input[type="file"]').change(controller.fileInputChange);
     $('#button-next').click(controller.nextButtonClick);
     $('#button-submit').click(controller.submitButtonClick);
-
-    $('#vpn_auth_type').find('input').change(controller.authtypeChbxChange);
+    $('#ynh_domain').blur(controller.dynetteCheckingBlur);
+    $('#vpn_ip6_net').blur(controller.vpnIp6NetBlur);
+    $('#ynh_password').blur(controller.ynhPasswordBlur);
+    $('#vpn_auth_type').find('input').change(controller.vpnAuthTypeChange);
   }
 };
 
@@ -277,7 +280,7 @@ var view = {
 /*******************/
 
 var controller = {
-  authtypeChbxChange: function() {
+  vpnAuthTypeChange: function() {
     var name = $(this).data('auth');
   
     if($(this).is(':checked')) {
@@ -328,6 +331,12 @@ var controller = {
     navigation.goToStep(nextStep);
   },
 
+  formNextSubmit: function() {
+    if($('#button-next').is(':visible')) {
+      $('#button-next').click();
+    }
+  },
+
   submitButtonClick: function() {
     if(validation.form()) {
       controller.formSubmit();
@@ -339,7 +348,59 @@ var controller = {
     hypercube.toJson(hypercube.proposeDownload);
 
     return false;
-  } 
+  },
+
+  dynetteCheckingBlur: function() {
+    var dynette = $('.dynette');
+    var dynetteText = dynette.find('span');
+
+    dynette.removeClass('available');
+    dynette.removeClass('notavailable');
+    dynette.removeClass('dynetterror');
+
+    if($(this).val().trim().match(/\.nohost\.me$/) || $(this).val().trim().match(/\.noho\.st$/)) {
+      $.ajax({
+        url: 'http://dyndns.yunohost.org/test/' + $(this).val().trim(),
+        crossDomain: true,
+
+        error: function(jqXHR, textStatus, errorThrown) {
+          dynette.addClass('dynetterror');
+          dynetteText.text(_("Cannot check the domain availability"));
+
+          dynette.hide();
+          dynette.fadeIn();
+        },
+
+        success: function(data) {
+          var msg = $.parseJSON(data.responseText);
+  
+          if(msg['error']) {
+            dynette.addClass('notavailable');
+            dynetteText.text(_("This domain is not available"));
+          } else {
+            dynette.addClass('available');
+            dynetteText.text(_("This domain is available"));
+          }
+  
+          dynette.hide();
+          dynette.fadeIn();
+        }
+      });
+    }
+  },
+
+  vpnIp6NetBlur: function() {
+    if(!$('#hotspot_ip6_net').val().trim()) {
+      $('#hotspot_ip6_net').val($(this).val().trim());
+    }
+  },
+
+  ynhPasswordBlur: function() {
+    if(!$('#ynh_user_password').val().trim()) {
+      $('#ynh_user_password').val($(this).val().trim());
+    }
+  }
+
 };
 
 var navigation = {
@@ -423,136 +484,140 @@ var navigation = {
 };
 
 var validation = {
-  resetWarnings: function(panel) {
-    var warnings = $('#panel-' + panel + ' .alert-danger');
-    var step = /^vpn-/.test(panel) ? 'vpn' : panel;
+  warnings: {
+    reset: function(panel) {
+      var warnings = $('#panel-' + panel + ' .alert-danger');
+      var step = panel.match(/^vpn-/) ? 'vpn' : panel;
 
-    $('#timeline a[data-tab=' + step + ']').parent().addClass('warnings');
-    $('#panel-' + panel + ' .control-label').css('color', 'white');
-    $('#panel-' + panel + ' .hasWarnings').removeClass('hasWarnings');
+      $('#timeline a[data-tab=' + step + ']').parent().addClass('warnings');
+      $('#panel-' + panel + ' .control-label').css('color', 'white');
+      $('#panel-' + panel + ' .hasWarnings').removeClass('hasWarnings');
 
-    warnings.each(function(i) {
-      if(i == 0) {
-        $(this).hide();
-        $(this).fadeIn();
-      } else {
-        if($(this).closest('.form-group')) {
-          $(this).closest('.form-group').remove();
+      warnings.each(function(i) {
+        if(i == 0) {
+          $(this).hide();
+          $(this).fadeIn();
         } else {
-          $(this).remove();
+          if($(this).closest('.form-group')) {
+            $(this).closest('.form-group').remove();
+          } else {
+            $(this).remove();
+          }
         }
+      });
+    },
+
+    add: function(field, msg) {
+      var warnings = $('.alert-danger').first().clone();
+      var warnMsg = warnings.find('strong');
+
+      warnMsg.text(msg);
+
+      var formGroup = $('<div>', { class: 'form-group alert-input' });
+      var label = $('<label>', { class: 'col-sm-3' });
+      var col = $('<div>', { class: 'col-sm-9' });
+
+      label.appendTo(formGroup);
+      col.appendTo(formGroup);
+      warnings.appendTo(col);
+
+      var tab = $('#' + field).closest('.tab');
+
+      if(tab) {
+        $('a[data-tab=' + tab.attr('id') + ']').addClass('hasWarnings');
       }
-    });
-  },
 
-  addWarning: function(field, msg) {
-    var warnings = $('.alert-danger').first().clone();
-    var warnMsg = warnings.find('strong');
+      $('#' + field).closest('.form-group').before(formGroup);
+      warnings.fadeIn();
+    },
 
-    warnMsg.text(msg);
+    none: function(panel) {
+      var warnings = $('#panel-' + panel + ' .alert ul');
+      var step = panel.match(/^vpn-/) ? 'vpn' : panel;
 
-    var formGroup = $('<div>', { class: 'form-group alert-input' });
-    var label = $('<label>', { class: 'col-sm-3' });
-    var col = $('<div>', { class: 'col-sm-9' });
+      $('#timeline a[data-tab=' + step + ']').parent().removeClass('warnings');
+      $('#timeline a[data-tab=' + step + ']').parent().addClass('validated');
 
-    label.appendTo(formGroup);
-    col.appendTo(formGroup);
-    warnings.appendTo(col);
-
-    var tab = $('#' + field).closest('.tab');
-
-    if(tab) {
-      $('a[data-tab=' + tab.attr('id') + ']').addClass('hasWarnings');
+      $('#panel-' + panel + ' .control-label').css('color', 'white');
+      warnings.parent().hide();
     }
-
-    $('#' + field).closest('.form-group').before(formGroup);
-    warnings.fadeIn();
   },
 
-  noMoreWarnings: function(panel) {
-    var warnings = $('#panel-' + panel + ' .alert ul');
-    var step = /^vpn-/.test(panel) ? 'vpn' : panel;
+  helpers: {
+    testMandatoryFields: function(fields) {
+      var nbWarns = 0;
 
-    $('#timeline a[data-tab=' + step + ']').parent().removeClass('warnings');
-    $('#timeline a[data-tab=' + step + ']').parent().addClass('validated');
+      $.each(fields, function(i, id) {
+        if(!$('#' + id).val().trim()) {
+          validation.warnings.add(id, _("This field is mandatory"));
+          nbWarns++;
+        }
+      });
 
-    $('#panel-' + panel + ' .control-label').css('color', 'white');
-    warnings.parent().hide();
-  },
+      return !nbWarns;
+    },
 
-  testMandatoryFields: function(fields) {
-    var nbWarns = 0;
+    testIpFields: function(fields, ipVersion) {
+      var nbWarns = 0;
 
-    $.each(fields, function(i, id) {
-      if(!$('#' + id).val().trim()) {
-        validation.addWarning(id, _("This field is mandatory"));
-        nbWarns++;
-      }
-    });
+      $.each(fields, function(i, id) {
+        var isWarn = false;
 
-    return !nbWarns;
-  },
+        if(!$('#' + id).val().trim()) {
+          return;
+        }
 
-  testIpFields: function(fields, ipVersion) {
-    var nbWarns = 0;
+        switch(ipVersion) {
+          case 64:
+            if(!ipaddr.isValid($('#' + id).val().trim())) {
+              validation.warnings.add(id, _("This IP format looks bad"));
+              isWarn = true;
+            }
+          break;
 
-    $.each(fields, function(i, id) {
-      var isWarn = false;
+          case 4:
+            if(!ipaddr.IPv4.isValid($('#' + id).val().trim())) {
+              validation.warnings.add(id, _("This IPv4 format looks bad"));
+              isWarn = true;
+            }
+          break;
 
-      if(!$('#' + id).val().trim()) {
-        return;
-      }
+          case 6:
+            if(!ipaddr.IPv6.isValid($('#' + id).val().trim())) {
+              validation.warnings.add(id, _("This IPv6 format looks bad"));
+              isWarn = true;
+            }
+          break;
+        }
 
-      switch(ipVersion) {
-        case 64:
-          if(!ipaddr.isValid($('#' + id).val())) {
-            validation.addWarning(id, _("This IP format looks bad"));
-            isWarn = true;
-          }
-        break;
+        if(!isWarn) {
+          $('#' + id).val(ipaddr.parse($('#' + id).val().trim()).toString());
+        } else {
+          nbWarns++;
+        }
+      });
 
-        case 4:
-          if(!ipaddr.IPv4.isValid($('#' + id).val())) {
-            validation.addWarning(id, _("This IPv4 format looks bad"));
-            isWarn = true;
-          }
-        break;
-
-        case 6:
-          if(!ipaddr.IPv6.isValid($('#' + id).val())) {
-            validation.addWarning(id, _("This IPv6 format looks bad"));
-            isWarn = true;
-          }
-        break;
-      }
-
-      if(!isWarn) {
-        $('#' + id).val(ipaddr.parse($('#' + id).val()).toString());
-      } else {
-        nbWarns++;
-      }
-    });
-
-    return !nbWarns;
+      return !nbWarns;
+    }
   },
 
   form: function() {
-    validation.resetWarnings('download');
+    validation.warnings.reset('download');
 
     if(!validation.vpn() || !validation.postinstall()) {
-      validation.addWarning('download', _("The configuration files cannot be generated because some steps still have warnings."));
-      validation.addWarning('download', _("Please fix them, then retry."));
+      validation.warnings.add('download', _("The configuration files cannot be generated because some steps still have warnings."));
+      validation.warnings.add('download', _("Please fix them, then retry."));
 
       return false;
     }
 
-    validation.noMoreWarnings('download');
+    validation.warnings.none('download');
 
     return true;
   },
 
   aboutyou: function() {
-    validation.noMoreWarnings('aboutyou');
+    validation.warnings.none('aboutyou');
 
     return true;
   },
@@ -569,17 +634,15 @@ var validation = {
 
   vpnAuto: function() {
     var nbWarns = 0;
-    validation.resetWarnings('vpn-auto');
+    validation.warnings.reset('vpn-auto');
 
-    var files = $('#vpn_cubefile').prop('files');
-
-    if(files.length == 0) {
+    if(!$('#vpn_cubefile').val().trim()) {
       nbWarns++;
-      validation.addWarning('vpn_cubefile', _("No .cube file selected"));
+      validation.warnings.add('vpn_cubefile', _("No .cube file selected"));
     }
 
     if(nbWarns == 0) {
-      validation.noMoreWarnings('vpn-auto');
+      validation.warnings.none('vpn-auto');
 
       return true;
     }
@@ -589,7 +652,7 @@ var validation = {
 
   vpnManual: function() {
     var nbWarns = 0;
-    validation.resetWarnings('vpn-manual');
+    validation.warnings.reset('vpn-manual');
 
     var ip6Fields = [ 'vpn_ip6_net' ];
     var ipFields = [ 'vpn_dns0', 'vpn_dns1' ];
@@ -609,25 +672,25 @@ var validation = {
       mandatoryFields.push('vpn_crt_client_ta');
     }
 
-    if(!validation.testMandatoryFields(mandatoryFields)) {
+    if(!validation.helpers.testMandatoryFields(mandatoryFields)) {
       nbWarns++;
     }
 
-    if(!validation.testIpFields(ip6Fields, 6)) {
+    if(!validation.helpers.testIpFields(ip6Fields, 6)) {
       nbWarns++;
     }
 
-    if(!validation.testIpFields(ipFields, 64)) {
+    if(!validation.helpers.testIpFields(ipFields, 64)) {
       nbWarns++;
     }
 
-    if(!/^[0-9]+/.test($('#vpn_server_port').val())) {
-      validation.addWarning('vpn_server_port', _("A Port is only composed of digits"));
+    if($('#vpn_server_port').val().trim() && !$('#vpn_server_port').val().match(/^[0-9]+/)) {
+      validation.warnings.add('vpn_server_port', _("Must be only composed of digits"));
       nbWarns++;
     }
 
     if(nbWarns == 0) {
-      validation.noMoreWarnings('vpn-manual');
+      validation.warnings.none('vpn-manual');
 
       return true;
     }
@@ -636,7 +699,92 @@ var validation = {
   },
 
   postinstall: function() {
-    return true;
+    var nbWarns = 0;
+    validation.warnings.reset('postinstall');
+
+    var ip6Fields = [ 'hotspot_ip6_net', 'hotspot_ip6_dns0', 'hotspot_ip6_dns1' ];
+    var ip4Fields = [ 'hotspot_ip4_dns0', 'hotspot_ip4_dns1' ];
+
+    var mandatoryFields = [ 'ynh_user', 'ynh_password', 'ynh_domain', 'hotspot_wifi_ssid',
+      'hotspot_wifi_passphrase', 'ynh_user_firstname', 'ynh_user_lastname', 'ynh_user_password',
+      'hotspot_ip6_dns0', 'hotspot_ip6_dns1', 'hotspot_ip4_dns0', 'hotspot_ip4_dns1', 'hotspot_ip4_nat_prefix' ];
+
+    if(!validation.helpers.testMandatoryFields(mandatoryFields)) {
+      nbWarns++;
+    }
+
+    if(!validation.helpers.testIpFields(ip6Fields, 6)) {
+      nbWarns++;
+    }
+
+    if(!validation.helpers.testIpFields(ip4Fields, 4)) {
+      nbWarns++;
+    }
+
+    var ip4_nat_prefix = $('#hotspot_ip4_nat_prefix').val().trim();
+
+    if(ip4_nat_prefix) {
+      var privateRanges = [ '10.0.0.0', '172.16.0.0', '192.168.0.0' ];
+      var cidrRanges = [ 8, 12, 16 ];
+      var isMatchPrivateRange = false;
+
+      ip4_nat_prefix = ip4_nat_prefix.trim().replace(/^([0-9]+\.[0-9]+\.[0-9]+).0$/, '$1');
+      $('#hotspot_ip4_nat_prefix').val(ip4_nat_prefix);
+      ip4_nat_prefix += '.0';
+
+      if(!ipaddr.IPv4.isValid(ip4_nat_prefix)) {
+        validation.warnings.add('hotspot_ip4_nat_prefix', _("This IPv4 prefix looks bad (required format is x.x.x or x.x.x.0)"));
+        nbWarns++;
+
+      } else {
+        $.each(privateRanges, function(i, range) {
+          var addr = ipaddr.parse(ip4_nat_prefix);
+
+          if(addr.match(ipaddr.parse(range), cidrRanges[i])) {
+            isMatchPrivateRange = true;
+          }
+        });
+
+        if(!isMatchPrivateRange) {
+          validation.warnings.add('hotspot_ip4_nat_prefix', _("This prefix must be part of an IPv4 private range"));
+          nbWarns++;
+        }
+      }
+    }
+
+    if($('#ynh_user').val().trim()) {
+      if(!$('#ynh_user').val().trim().match(/^[a-z0_9]+$/)) {
+        validation.warnings.add('ynh_user', _("Only lowercase letters and digits are allowed"));
+        nbWarns++;
+  
+      } else if($('#ynh_user').val().trim() == 'admin') {
+        validation.warnings.add('ynh_user', _("Admin is not an authorized value"));
+        nbWarns++;
+      }
+    }
+
+    if($('#ynh_password').val().trim() && !$('#ynh_password').val().trim().length < 4) {
+      validation.warnings.add('ynh_password', _("Must be greater than 4 characters"));
+      nbWarns++;
+    }
+
+    if($('#hotspot_wifi_passphrase').val().trim() && !$('#hotspot_wifi_passphrase').val().trim().match(/^[ -~]+$/)) {
+      validation.warnings.add('hotspot_wifi_passphrase', _("Only printable ASCII characters are allowed"));
+      nbWarns++;
+    }
+
+    if($('#hotspot_wifi_passphrase').val().trim() && $('#hotspot_wifi_passphrase').val().trim().length < 3 || $('#hotspot_wifi_passphrase').val().trim().length > 63) {
+      validation.warnings.add('hotspot_wifi_passphrase', _("Must from 8 to 63 characters (WPA2 passphrase)"));
+      nbWarns++;
+    }
+
+    if(nbWarns == 0) {
+      validation.warnings.none('postinstall');
+
+      return true;
+    }
+
+    return false;
   }
 };
 
