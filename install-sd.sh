16e3698dbb41bdac64008bc579e405617729ed87
Julien Vaubourg <julien@vaubourg.com>
2016-02-28 12:32:10 +0100

    Add autoconnect

--- install-sd.sh
+++ install-sd.sh
@@ -246,6 +246,7 @@ function find_cubes() {
   local ips=$(sudo arp-scan --local | grep -P '\t02' | awk '{ print $1 }')
   local addip=
   local addhost=
+  local knownhosts=0
   local i=0
 
   if [ -z "${ips}" ]; then
@@ -259,37 +260,55 @@ function find_cubes() {
     i=$(( i + 1 ))
 
     knownhost=$(awk "/$ip/ { print \$2 }" /etc/hosts | head -n1)
-    [ -z "${knownhost}" ] && knownhost=$ip
 
-    echo -e "  ${i}. ${ip} | ssh root@${knownhost} | https://${knownhost}"
+    if [ -z "${knownhost}" ]; then
+      knownhost=$ip
+    else
+      (( knownhosts++ )) || true
+    fi
+
+    echo "  ${i}. ${ip} | ssh root@${knownhost} | https://${knownhost}"
   done
 
-  echo -en "\nSelect an IP to add to your hosts file (just press Enter if not necessary): "
-  read addip
+  echo
 
-  if [ -z "${addip}" ]; then
-    exit_normal
-  fi
-
-  if [[ "${addip}" =~ ^[0-9]+$ ]]; then
-    addip=${ips[$(( addip - 1 ))]}
+  if [ "${knownhosts}" -ne "${#ips[@]}" ]; then
+    echo -n "Select an IP to add to your hosts file (just press Enter if not necessary): "
+    read addip
 
     if [ -z "${addip}" ]; then
-      exit_error "IP index not found"
+      exit_normal
     fi
+
+    if [[ "${addip}" =~ ^[0-9]+$ ]]; then
+      addip=${ips[$(( addip - 1 ))]}
+  
+      if [ -z "${addip}" ]; then
+        exit_error "IP index not found"
+      fi
+    fi
+  
+    if [[ ! "${addip}" =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
+      exit_error "This is not an IPv4 nor an IP index"
+    fi
+  
+    echo -en "Choose a host name for this IP: "
+    read addhost
+  
+    echo -e "${addip}\t${addhost}" | sudo tee -a /etc/hosts > /dev/null
+  
+    info "IP successfully added to your hosts file"
+    echo -e "\n  ssh root@${addhost} | https://${addhost}\n"
+
+  elif [ "${knownhosts}" -eq 1 ]; then
+    echo "% ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@${knownhost} 2> /dev/null"
+    echo "Press Enter to execute (or Ctrl-C to arbort)"
+    read
+
+    echo "Default Password: olinux"
+    ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no "root@${knownhost}" 2> /dev/null
+    exit_normal
   fi
-
-  if [[ ! "${addip}" =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
-    exit_error "This is not an IPv4 nor an IP index"
-  fi
-
-  echo -en "Choose a host name for this IP: "
-  read addhost
-
-  echo -e "${addip}\t${addhost}" | sudo tee -a /etc/hosts > /dev/null
-
-  info "IP successfully added to your hosts file"
-  echo -e "\n  ssh root@${addhost} | https://${addhost}\n"
 }
 
 function autodetect_sdcardpath() {
