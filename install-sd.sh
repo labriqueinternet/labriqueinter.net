b02d2141a888c68f24767d13c1702a97202a1ea0
Julien Vaubourg <julien@vaubourg.com>
2016-08-20 20:50:41 +0200

    Add sync option to dd for (maybe) avoiding non-dectected partition

--- install-sd.sh
+++ install-sd.sh
@@ -646,7 +646,7 @@ function install_encrypted() {
   fi
 
   debug "Raw copying ${sunxispl_path} to ${opt_sdcardpath} (dd)"
-  sudo dd "if=${sunxispl_path}" "of=${opt_sdcardpath}" bs=1024 seek=8 &> /dev/null
+  sudo dd "if=${sunxispl_path}" "of=${opt_sdcardpath}" bs=1M seek=8 conv=fsync oflag=nocache,sync &> /dev/null
 
   debug "Flushing file system buffers (sync)"
   sudo sync
@@ -660,7 +660,7 @@ function install_clear() {
   info "Please wait..."
 
   debug "Raw copying ${img_path} to ${opt_sdcardpath} (dd)"
-  sudo dd if="${img_path}" of="${opt_sdcardpath}" bs=1M &> /dev/null
+  sudo dd if="${img_path}" of="${opt_sdcardpath}" bs=1M conv=fsync oflag=nocache,sync &> /dev/null
 
   debug "Flushing file system buffers"
   sudo sync
